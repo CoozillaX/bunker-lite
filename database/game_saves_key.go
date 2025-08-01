52e8080992ae8ecb4279c49e25e265bf3d1378c8
Happy2018new <chenbo79800@163.com>
2025-08-01 19:53:29 +0800

    minor changes

--- database/game_saves_key.go
+++ database/game_saves_key.go
@@ -17,14 +17,14 @@ func GetOrCreateGameSavesKey(eulogistUniqueID string, rentalServerNumber string,
 		defer mu.Unlock()
 	}
 
-	key := define.GameSavesKey{
+	gameSavesKey := define.GameSavesKey{
 		EulogistUserUniqueID: eulogistUniqueID,
 		RentalServerNumber:   rentalServerNumber,
 	}
 
 	keyBuf := bytes.NewBuffer(nil)
 	writer := protocol.NewWriter(keyBuf, 0)
-	key.MarshalKey(writer)
+	gameSavesKey.MarshalKey(writer)
 
 	err = serverDatabase.Update(func(tx *bbolt.Tx) error {
 		bucket := tx.Bucket([]byte(DATABASE_KEY_GAME_SAVES_KEYS))
@@ -32,18 +32,18 @@ func GetOrCreateGameSavesKey(eulogistUniqueID string, rentalServerNumber string,
 
 		if len(payload) == 0 {
 			uniqueID := uuid.New()
-			key.GameSavesAESCipher = uniqueID[:]
+			gameSavesKey.GameSavesAESCipher = uniqueID[:]
 
 			dataBuf := bytes.NewBuffer(nil)
 			writer = protocol.NewWriter(dataBuf, 0)
-			key.MarshalData(writer)
+			gameSavesKey.MarshalData(writer)
 
 			return bucket.Put(keyBuf.Bytes(), dataBuf.Bytes())
 		}
 
 		dataBuf := bytes.NewBuffer(payload)
 		reader := protocol.NewReader(dataBuf, 0, false)
-		key.MarshalData(reader)
+		gameSavesKey.MarshalData(reader)
 
 		return nil
 	})
@@ -51,5 +51,5 @@ func GetOrCreateGameSavesKey(eulogistUniqueID string, rentalServerNumber string,
 		return nil, fmt.Errorf("GetOrCreateGameSavesKey: 获取存档 AES 密钥时出现问题，原因是 %v", err)
 	}
 
-	return key.GameSavesAESCipher, nil
+	return gameSavesKey.GameSavesAESCipher, nil
 }
